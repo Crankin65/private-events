23dc38a921b7f18f3bca1321d3e78308ae4637c9
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-11-18 15:51:23 -0500

    Changed migration file

--- db/migrate/20221118203328_create_events.rb
+++ db/migrate/20221118203328_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.string :name
-      t.event_date :date
+      t.date :event_date
       t.timestamps
     end
   end
