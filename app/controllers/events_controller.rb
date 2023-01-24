205a11d9dabc12a693a6a3dd6b932d853e37b505
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-01-24 13:49:19 -0500

    Updated creator and events controller, created events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,41 @@
 class EventsController < ApplicationController
 
-  def index 
+  def index
     @events = Event.all
   end
+
+  def show
+
+  end
+
+  def new
+    @event = Event.new
+  end
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: "Your event was created"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event, notice: "Your event was updated"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "events#index"
+  get "/events/create", to: 'events#create'
+  resources :events
+  resources :users, only: :index
+
 end
