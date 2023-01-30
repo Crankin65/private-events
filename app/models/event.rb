fbef2832ba04eeb4c7bff96c8afff62811757bb8
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-01-30 13:20:37 -0500

    updated index page for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
 
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, foreign_key: "user_id", class_name: "User", primary_key: :id
   has_many :attended_events
   has_many :attendees, through: :attended_events, source: :user
 end
