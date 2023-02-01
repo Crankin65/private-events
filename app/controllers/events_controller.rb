45814f5f7e912442357fc4435e1e67ec824c365c
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-01 12:34:14 -0500

    Created query to show future events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,7 +44,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :description, :date)
+    params.require(:event).permit(:title, :body, :date)
   end
 
 
